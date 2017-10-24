ae2edfa8bf7e750099d9ae5ff37cd39f063a6b6e
Vadim Gensitsky <Vadim Gensitsky>
2017-10-24 11:20:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*Дано целое число N большее 0, найти число, получен-
+ное при прочтении числа N справа налево. Например,
+если было введено число 345, то программа должна
+вывести число 543.*/
+using System;
+
+namespace HomeCaseCS_Lesson1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N <= 0)
+            {
+                Console.WriteLine("Error! Enter positive numbers " +
+                    "greater than 0");
+            }
+            else
+            {
+                while (N > 0)
+                {
+                    Console.Write(N % 10);
+                    N /= 10;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
